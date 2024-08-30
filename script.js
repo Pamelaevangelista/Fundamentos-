c1a92b53540e3f5ad244273aefe769b3898fe5ec
Pamelaevangelista <panuemanuelecilva@gmail.com>
2024-08-29 21:28:47 -0300

    Aula 1

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Olá,Mundo");
+alert("Hello, Word");
+/*let preco=prompt("digite um preço");
+Document.write(preco)*/
+
+let nome = prompt("Digite o seu nome")
+document.write ("Olá ",nome,".Seja bem-vindex ao site");
